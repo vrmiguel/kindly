3fc35e239bd6e0e612993cf36718024487edc603
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-11-10 20:47:20 -0300

    Set the exit code accordingly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use command::run_command;
-
 mod command;
 mod drop_zeroed;
 mod error;
 
+use command::run_command;
+use error::Result;
+
+fn try_main() -> Result<i32>{
+    let status = run_command(argv::iter().skip(1))?;
+
+    if !status.is_ok() {
+        println!("[kindly] {}", status);
+    }
+
+    Ok(status.code_or_signal())
+}
+
 fn main() {
-    run_command(argv::iter().skip(1));
+    match try_main() {
+        Ok(code_or_signal) => std::process::exit(code_or_signal),
+        Err(err) => {
+            println!("[kindly] {}", err);
+            std::process::exit(127);
+        }
+    }
 }
